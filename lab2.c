a07ea2a5c7bf7cb6aaca8c6f9c1c71c202b587fe
af3ld <alexfeldman@lclark.edu>
2015-10-18 13:54:59 -0700

    cleaned the welcome intro

--- lab2.c
+++ lab2.c
@@ -121,6 +121,19 @@ void trans_scale_trans(int i, double sf,  double m[3][3], double minv[3][3]){
 }
 
 
+//writes the opening instructions
+void welcome(int i){
+  printf("Press");
+  if (i == 1){
+    printf(" 1 to start: ");
+  } else if (i == 2){
+    printf(" 1 or 2 to start: ");
+  } else {
+    printf(" 1 through %d to begin: ", i);
+  }
+}
+
+
 
 int main(int argc, char **argv){
   char key, c;  FILE *g;
@@ -139,9 +152,9 @@ int main(int argc, char **argv){
 
       }
     }
-
   
-  printf("Which item you would like to start with: ");
+  welcome(argc - 1);
+  
   scanf("%c", &key);
   int i = key - '0';
   

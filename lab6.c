13115d8bef2fd971332d44a41a7da02029179a4f
af3ld <alexfeldman@lclark.edu>
2015-11-19 00:24:41 -0800

    Strange things are a foot when dealing with large amounts of points and such

--- lab6.c
+++ lab6.c
@@ -1,22 +1,27 @@
 #include <FPT.h>
 #include <D3d_matrix.h>
 
-double centerx, centery, centerz;
 double halfangle = 40;
-double x[100][100], y[100][100], z[100][100]; //x, y, z coordinates
-int points[100]; //number of points in whatever
-int polys[10];
-int shapeorder[100][100][100]; //where things connect to
-int shapes[100][100]; //the shapes[what argument][which shape]
+double radians = 3 * (M_PI / 180);
 int WIDTH = 600; int HEIGHT = 600; int DEPTH = 600;
 
 typedef struct {
+  double centerx, centery, centerz;
+  double x[10000], y[10000], z[10000];
+  FILE *file;
+  int numpolys; //the number of subpolygons that make up the object
+  int points; //number of points in whatever
+  int shapeorder[100][100]; //Where things connect to
+  int shapes[1000]; //the shapes [which shape]
+} Polygon;
+
+typedef struct {
+  double avg_depth;
   double plane_x[100];
   double plane_y[100];
   double plane_z[100];
-  double avg_depth;
-  int size;
   double r, g, b;
+  int size;
 } Plane;
 
 void printarray(Plane *a, int size) {
@@ -29,22 +34,23 @@ void printarray(Plane *a, int size) {
 
 //reads in file
 //Modified from lab2; does not read in colors
-void readobject(FILE *g, int i) {
+void readobject(FILE *g, Polygon* poly) {
   int k, j;
-  fscanf(g, "%d", &points[i]);
-  // printf("there are %d points\n", points[i]);
-  for (k = 0; k < points[i]; k++) {
-    fscanf(g, "%lf  %lf  %lf", &x[i][k], &y[i][k], &z[i][k]);
-    // printf("(%lf,%lf,%lf)\n", x[i][k], y[i][k], z[i][k]);
+  fscanf(g, "%d", &poly->points);
+  printf("there are %d points\n", poly->points);
+  for (k = 0; k < poly->points; k++) {
+    fscanf(g, "%lf  %lf  %lf", &poly->x[k], &poly->y[k],
+           &poly->z[k]);
+    // printf("(%lf,%lf,%lf)\n", poly->x[k], poly->y[k], poly->z[k]);
   }
-  fscanf(g, "%d", &polys[i]);
-  // printf("there are %d polygons in this whatever\n", polys[i]);
-  for (k = 0; k < polys[i]; k++) {
-    fscanf(g, "%d", &shapes[i][k]);
-    // printf("%d: ", shapes[i][k]);
-    for (j = 0; j < shapes[i][k]; j++) {
-      fscanf(g, "%d", &shapeorder[i][k][j]);
-      // printf("%d ", shapeorder[i][k][j]);
+  fscanf(g, "%d", &poly->numpolys);
+  // printf("there are %d polygons in this whatever\n", poly->numpolys);
+  for (k = 0; k < poly->numpolys; k++) {
+    fscanf(g, "%d", &poly->shapes[k]);
+    // printf("%d: ", poly->shapes[k]);
+    for (j = 0; j < poly->shapes[k]; j++) {
+      fscanf(g, "%d", &poly->shapeorder[k][j]);
+      // printf("%d ", poly->shapeorder[k][j]);
     }
     // printf("\n");
   }
@@ -59,26 +65,24 @@ int compare (const void *p, const void *q) {
          ((*a).avg_depth) > ((*b).avg_depth) ? -1 : 0;
 }
 
-void drawit(int i) {
+void drawit(Polygon poly) {
   int k, j;
-  double plane_x[points[i]], plane_y[points[i]], plane_z[points[i]];
   double mod = (HEIGHT / 2) / tan(halfangle * (M_PI / 180));
-  Plane planecontainer[polys[i]];
-
-  for (k = 0; k < polys[i]; k++) {
+  Plane planecontainer[poly.numpolys];
+  for (k = 0; k < poly.numpolys; k++) {
     j = 0;
     planecontainer[k].avg_depth = 0.0;
-    while (j < shapes[i][k]) {
-      // printf("%d", shapeorder[i][k][j]);
-      if (fabs(z[i][shapeorder[i][k][j]]) > 10e-7) { //Checks if z == 0
-        planecontainer[k].plane_x[j] = mod * (x[i][shapeorder[i][k][j]]
-                                              / z[i][shapeorder[i][k][j]])
+    while (j < poly.shapes[k]) {
+      if (fabs(poly.z[poly.shapeorder[k][j]]) > 10e-7) { //Checks if z == 0
+        planecontainer[k].plane_x[j] = mod * (poly.x[poly.shapeorder[k][j]]
+                                              / poly.z[poly.shapeorder[k][j]])
                                        + (WIDTH / 2);
-        planecontainer[k].plane_y[j] = mod * (y[i][shapeorder[i][k][j]]
-                                              / z[i][shapeorder[i][k][j]])
-                                       + (HEIGHT / 2);
-        planecontainer[k].plane_z[j] = z[i][shapeorder[i][k][j]];
-        planecontainer[k].avg_depth += z[i][shapeorder[i][k][j]];
+        planecontainer[k].plane_y[j] = mod * (poly.y[poly.shapeorder[k][j]]
+                                              / poly.z[poly.shapeorder[k][j]])
+                                       + (WIDTH / 2);
+        printf("%lf, %lf\n", planecontainer[k].plane_x[j], planecontainer[k].plane_y[j]);
+        planecontainer[k].plane_z[j] = poly.z[poly.shapeorder[k][j]];
+        planecontainer[k].avg_depth += poly.z[poly.shapeorder[k][j]];
         j++;
       } else {
         j++;
@@ -90,11 +94,11 @@ void drawit(int i) {
     planecontainer[k].g = (double)(rand() % 100) / 100;
     planecontainer[k].b = (double)(rand() % 100) / 100;
   }
-  // printarray(planecontainer, polys[i]);
-  qsort (planecontainer, polys[i], sizeof(Plane), compare);
-  // printarray(planecontainer, polys[i]);
+  // printarray(planecontainer, poly.numpolys);
+  qsort (planecontainer, poly.numpolys, sizeof(Plane), compare);
+  // printarray(planecontainer, poly.numpolys);
 
-  for (k = 0; k < polys[i]; k++) {
+  for (k = 0; k < poly.numpolys; k++) {
     G_rgb(planecontainer[k].r, planecontainer[k].g, planecontainer[k].b);
     G_fill_polygon(planecontainer[k].plane_x,
                    planecontainer[k].plane_y, planecontainer[k].size);
@@ -120,24 +124,26 @@ int findextrema(double *y, int z, int swatch) {
 
 //finds the scale factor of the object
 //also reveals the center of the object
-double scale_n_fit(int i) {
+double scale_n_fit(Polygon *poly) {
   int k;
   double smallx, bigx, smally, bigy, smallz, bigz;
-  bigx = x[i][findextrema(x[i], points[i], 0)];
-  smallx = x[i][findextrema(x[i], points[i], 1)];
-  bigy = y[i][findextrema(y[i], points[i], 0)];
-  smally = y[i][findextrema(y[i], points[i], 1)];
-  bigz = z[i][findextrema(z[i], points[i], 0)];
-  smallz = z[i][findextrema(z[i], points[i], 1)];
+  bigx = poly->x[findextrema(poly->x, poly->points, 0)];
+  smallx = poly->x[findextrema(poly->x, poly->points, 1)];
+  bigy = poly->y[findextrema(poly->y, poly->points, 0)];
+  smally = poly->y[findextrema(poly->y, poly->points, 1)];
+  bigz = poly->z[findextrema(poly->z, poly->points, 0)];
+  smallz = poly->z[findextrema(poly->z, poly->points, 1)];
 
   double boxheight = bigy - smally;
   double boxwidth =  bigx - smallx;
   double boxdepth = bigz - smallz;
 
-  centerx = (bigx + smallx) / 2;
-  centery = (bigy + smally) / 2;
-  centerz = (bigz + smallz) / 2;
-  double centers[3] = {centerx, centery, centerz};
+  poly->centerx = (bigx + smallx) / 2;
+  poly->centery = (bigy + smally) / 2;
+  poly->centerz = (bigz + smallz) / 2;
+  double centers[3] = {poly->centerx, poly->centery,
+                       poly->centerz
+                      };
   return (findextrema(centers, 3, 1) == 1) ? WIDTH / boxwidth :
          (findextrema(centers, 3, 1) == 2) ? HEIGHT / boxheight :
          DEPTH / boxdepth;
@@ -145,49 +151,54 @@ double scale_n_fit(int i) {
 
 //changes the message based on how many objects are inputted via command line
 void welcome(int i) {
-  printf((i == 1) ? "Press 1 to start: " :
-         (i == 2) ? "Press 1 or 2 to start: " :
-         "Press 1 through %d to start ", i);
+  printf((i == 1) ? "Press 1 to move the object: " :
+         (i == 2) ? "Press 1 or 2 to the first or second object: " :
+         "Press 1 through %d to move the respective object: ", i);
 }
 
 int main (int argc, char **argv)
 {
   char q, action;
   double mat[4][4], minv[4][4], scaleFactor;
-  double radians = 3 * (M_PI / 180);
-  FILE *g;
   int cc, sign, currentObj, k, h;
-  int increment = 15;
+  int increment = 20;
   int xcounter, ycounter, zcounter = 0;
+  Polygon polygon[argc];
 
   for (cc = 1; cc < argc; cc++) {
-    g = fopen(argv[cc], "r"); //opens a file; r = read only
-    if (g == NULL) { //if the file is empty, it will let me know
+    polygon[cc].file = fopen(argv[cc], "r"); //opens a file; r = read only
+    if (polygon[cc].file == NULL) { //if the file is empty
       printf("can't open (1)\n");
       exit(1);
     } else {
-      readobject(g, cc);
-
+      readobject(polygon[cc].file, &polygon[cc]);
+      // printf("%d\n", polygon[cc].points);
       D3d_make_identity(mat); D3d_make_identity(minv);
-      scaleFactor = scale_n_fit(cc);
-      D3d_translate(mat, minv, -centerx, -centery, -centerz);
+      scaleFactor = scale_n_fit(&polygon[cc]);
+      D3d_translate(mat, minv, -polygon[cc].centerx, -polygon[cc].centery,
+                    -polygon[cc].centerz);
       D3d_scale(mat, minv, scaleFactor, scaleFactor, scaleFactor);
-      D3d_mat_mult_points(x[cc], y[cc], z[cc], mat,
-                          x[cc], y[cc], z[cc], points[cc]);
-      // printarray(z[cc], points[cc]);
+      D3d_mat_mult_points(polygon[cc].x, polygon[cc].y, polygon[cc].z,
+                          mat, polygon[cc].x, polygon[cc].y,
+                          polygon[cc].z, polygon[cc].points);
     }
   }
-  
+
+  // exit(1);
+  printf("Which object (between 1 and %d) would you like to start with? \n", argc - 1);
+  scanf("%c", &q);
   currentObj = q - '0';
   sign = 1 ;
   action = 't' ;
 
   if (currentObj < argc && currentObj > 0) {
     G_init_graphics(WIDTH, HEIGHT);
+
     while (1) {
       G_rgb(0, 0, 0);
       G_clear();
-      drawit(currentObj);
+      drawit(polygon[currentObj]);
+
 
       D3d_make_identity (mat) ;
       D3d_make_identity (minv) ;
@@ -241,15 +252,13 @@ int main (int argc, char **argv)
       }
 
 
-      D3d_mat_mult_points (x[currentObj], y[currentObj], z[currentObj],
-                           mat,  x[currentObj], y[currentObj],
-                           z[currentObj], points[currentObj] + 1) ;
+      D3d_mat_mult_points(polygon[currentObj].x, polygon[currentObj].y,
+                          polygon[currentObj].z, mat,
+                          polygon[currentObj].x, polygon[currentObj].y,
+                          polygon[currentObj].z,
+                          polygon[currentObj].points + 1) ;
       //the numpoints[currentObj]+1 is because we have stored
       //the center of the object at the arrays' end
-
-      G_rgb(0, 0, 0);
-      G_clear();
-      drawit(currentObj);
     }
   }
 }

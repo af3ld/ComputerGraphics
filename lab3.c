df3ee01f1891d42fe1608ba4265724b00968e883
af3ld <alexfeldman@lclark.edu>
2015-12-22 22:46:28 -0500

    fixing repo

---
+++ lab3.c
@@ -0,0 +1,315 @@
+#include <FPT.h>
+#include <D2d_matrix.h>
+
+int WIDTH = 500; int HEIGHT = 500;
+int counter = 0;
+int points[100]; //number of points in whatever
+double x[100][100], y[100][100]; //x and y coordinates
+int polys[10];
+int shapes[100][100]; //the shapes[what argument][which shape]
+int shapeorder[100][100][100]; //where things connect to
+double red[100][100], green[100][100], blue[100][100]; //individual colors of each[what arg][which shape]
+int boxheight, boxwidth, centerx, centery;
+double smallx, smally, bigx, bigy; //specific smallest & largest (X,Y)'s
+
+//determines if a point is on one side of the line or the other
+double whatside(double ax, double ay, double bx,
+                double by, double x1, double y1,
+                double x2, double y2) {
+
+  return ((y1 - y2) * (ax - x1) + (x2 - x1) * (ay - y1)) *
+         ((y1 - y2) * (bx - x1) + (x2 - x1) * (by - y1));
+}
+
+//Bounding Box For Line Segment
+//(x1,y1) = start of the line
+//(x2, y2) = the end of the line
+int pointincontainer(double x1, double y1, double x2, double y2,
+                     double xprime, double yprime) {
+  double left, top, right, bottom;
+  if (x1 < x2) {
+    left = x1;
+    right = x2;
+  } else {
+    left = x2;
+    right = x1;
+  }
+  if (y1 > y2) {
+    top = y1;
+    bottom = y2;
+  } else {
+    top = y2;
+    bottom = y1;
+  }
+  //printf("l:%lf, r:%lf, t:%lf, b:%lf\n", left, right, top, bottom);
+  //printf("x': %lf, y': %lf\n", xprime, yprime);
+  if ((xprime + 0.01) >= left && (xprime - 0.01) <= right &&
+      (yprime + 0.01) <= top && (yprime - 0.01) >= bottom) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int looper(int i, int n) {
+  if (i == n - 1) {
+    return 0;
+  } else {
+    return i + 1;
+  }
+}
+
+//gets the slope of all the lines; returns them via modified array
+void getslope(double *x, double *y, int z, double *slope) {
+  int i;
+  for (i = 0; i < z; i++) {
+    slope[i] = (y[looper(i, z)] - y[i]) / (x[looper(i, z)] - x[i]);
+  }
+}
+
+//finds the intersection of the all lines
+//returns intersection coordinates via array
+void f_intersect(double *px, double *py, int pn,
+                 double *wx, double *wy, int wn, double *c) {
+  int i, j;
+  double yprime, xprime, wslope[100], pslope[100], c1, c2;
+  getslope(px, py, pn, pslope);
+  getslope(wx, wy, wn, wslope);
+
+  for (i = 0; i < wn; i++) {
+    for (j = 0; j < pn; j++) {
+      c1 = py[j] - pslope[j] * px[j]; //y = mx + c - > c = y -mx
+      c2 = wy[i] - wslope[i] * wx[i];
+
+      if ((pslope[j] - wslope[i]) == 0) { //prevents divison by 0
+        printf("Slope division by 0; ERROR!\n");
+      } else {
+        xprime = (c2 - c1) / (pslope[j] - wslope[i]); //intersection of x
+        yprime = pslope[j] * xprime + c1; //intersection of y
+
+        if (pointincontainer(px[j], py[j], px[looper(j, pn)], py[looper(j, pn)],
+                             xprime, yprime) == 1 &&
+            pointincontainer(wx[i], wy[i], wx[looper(i, wn)], wy[looper(i, wn)],
+                             xprime, yprime ) == 1) {
+          c[counter] = xprime; c[counter + 1] = yprime;
+        G_wait_key();
+        G_rgb(0,0,0);
+        G_fill_circle(xprime,yprime, 2);
+          counter += 2;
+        }
+      }
+    }
+  }
+}
+
+
+//click a point and it saves it into an array
+int clickAndSave(double *x, double *y) {
+  double ca[2];
+  int i = 0;
+  int going = 1;
+  G_rgb(0, 0, 0);;
+  G_draw_string("Click here to end clipping", 0, 0);
+  while (going) {
+    G_wait_click(ca);
+    //printf("%lf, %lf\n", ca[0], ca[1]);
+    if (ca[0] < 260 & ca[1] < 15) {
+      going = 0;
+      x[i] = x[0];
+      y[i] = y[0];
+      //printf("done\n");
+    } else {
+      x[i] = ca[0];
+      y[i] = ca[1];
+      G_rgb(0, 1, 1);
+      G_circle(x[i], y[i], 1);
+      G_rgb(0, 0, 0);
+      G_circle(x[i], y[i], 2);
+      i++;
+      //printf(" %d\n", i);
+    }
+
+  }
+  return i;
+}
+
+//draws the polygon; unfilled
+void myPolygon(double *x, double *y, int n) {
+  int i = 0;
+  while (i <= n) {
+    if (i != n) {
+      G_line(x[i], y[i], x[i + 1], y[i + 1]);
+    }
+    i++;
+  }
+}
+
+//Creates bounding box around figure
+double boundingbox(int i) {
+  int k;
+
+  smallx = WIDTH * 2; smally = HEIGHT * 2;
+  bigx = WIDTH * -2; bigy = HEIGHT * -2;
+  for (k = 0; k < points[i]; k++) {
+    if (x[i][k] < smallx) {
+      smallx = x[i][k];
+    }
+    else if (x[i][k] > bigx) {
+      bigx = x[i][k];
+    }
+    if (y[i][k] < smally) {
+      smally = y[i][k];
+    }
+    else if (y[i][k] > bigy) {
+      bigy = y[i][k];
+    }
+  }
+
+  boxheight = bigy - smally;
+  boxwidth =  bigx - smallx;
+  centerx = (bigx + smallx) / 2;
+  centery = (smally + bigy) / 2;
+
+  if (WIDTH / boxwidth < HEIGHT / boxheight) {
+    return WIDTH / boxwidth;
+  } else {
+    return HEIGHT / boxheight;
+  }
+}
+
+
+//reads the file and attaches the correct variable to each input
+void readobject(FILE *g, int i) {
+  int k, j;
+
+  fscanf(g, "%d", &points[i]);
+  //printf("there are %d points\n", points[i]);
+  for (k = 0; k < points[i]; k++) {
+    fscanf(g, "%lf  %lf", &x[i][k], &y[i][k]);
+    //printf("(%lf,%lf)\n", x[i][k], y[i][k]);
+  }
+  fscanf(g, "%d", &polys[i]);
+  //printf("there are %d polygons in this whatever\n", polys[i]);
+
+  for (k = 0; k < polys[i]; k++) {
+    fscanf(g, "%d", &shapes[i][k]);
+    for (j = 0; j < shapes[i][k]; j++) {
+      fscanf(g, "%d", &shapeorder[i][k][j]);
+      //printf("%d\n", shapeorder[i][k][j]);
+    }
+  }
+  for (k = 0; k < polys[i]; k++) {
+    fscanf(g, "%lf %lf %lf", &red[i][k], &green[i][k], &blue[i][k]); //the rgb
+    //printf("(%lf, %lf, %lf)\n", red[i][k], green[i][k], blue[i][k]);
+  }
+}
+
+//draws the image
+void drawit(int i) {
+  int k, j;
+  double tempx[points[i]], tempy[points[i]];
+
+  for (k = 0; k < polys[i]; k++) {
+    for (j = 0; j < shapes[i][k]; j++) {
+      tempx[j] = x[i][shapeorder[i][k][j]];
+      tempy[j] = y[i][shapeorder[i][k][j]];
+    }
+
+    G_rgb(red[i][k], green[i][k], blue[i][k]);
+    G_fill_polygon(tempx, tempy, j);
+  }
+}
+
+//controls the movement of the object
+void movement(int i, double sf, double m[3][3], double minv[3][3]) {
+
+  D2d_translate(m, minv, -centerx, -centery);
+  D2d_scale(m, minv, sf, sf);
+  D2d_translate(m, minv, WIDTH / 2, HEIGHT / 2);
+  D2d_mat_mult_points(x[i], y[i], m, x[i], y[i], points[i]);
+}
+
+//writes the opening instructions
+void welcome(int i) {
+  printf("Press");
+  if (i == 1) {
+    printf(" 1 to start,");
+  } else if (i == 2) {
+    printf(" 1 or 2 to start,");
+  } else {
+    printf(" 1 through %d to begin,", i);
+  }
+  printf("\nthen press TAB to begin clipping\n");
+}
+
+//the clipping container
+void clipping(double *clipx, double *clipy, int i) {
+  double c[100];
+  int temp_i = clickAndSave(clipx, clipy);
+  myPolygon(clipx, clipy, temp_i);
+  f_intersect(clipx, clipy, temp_i, x[i], y[i], points[i], c);
+  G_wait_key();
+}
+
+int main(int argc, char **argv) {
+  char key, c;  FILE *g;
+  double m[3][3], minv[3][3], sf, clipx[100], clipy[100];
+  int cc;
+  int inner_cycle = 1;
+
+  for (cc = 1; cc < argc; cc++) {
+    g = fopen(argv[cc], "r"); //opens a file; r = read only
+    if (g == NULL) { //if the file is empty, it will let me know
+      printf("can't open (1)\n");
+      exit(1);
+    } else {
+      readobject(g, cc);
+
+      D2d_make_identity(m); D2d_make_identity(minv);
+      sf = boundingbox(cc); //Remember, sf == scale factor
+      movement(cc, sf, m, minv); //(arg, scale, m, m inverse)
+    }
+  }
+
+  welcome(argc - 1);
+  scanf("%c", &key);
+  int i = key - '0';
+
+  if (i < argc && i > 0) {
+    G_init_graphics(WIDTH, HEIGHT);
+
+    D2d_make_identity(m); D2d_make_identity(minv);
+    D2d_translate(m, minv, -WIDTH / 2, -HEIGHT / 2);
+    D2d_rotate(m, minv, M_PI / 45);
+    D2d_translate(m, minv, WIDTH / 2, HEIGHT / 2);
+
+    while (inner_cycle) {
+      G_rgb(1, 1, 1);
+      G_clear();
+
+      D2d_mat_mult_points(x[i], y[i], m, x[i], y[i], points[i]);
+
+      drawit(i);
+
+      G_rgb(0, 0, 0);
+
+      c = G_wait_key();
+      cc = c - '0';
+
+      if (cc > 0 && cc < argc) {
+        if (cc != i) {
+          i = cc;
+        }
+      } else if (cc == -39) {
+
+        inner_cycle = 0;
+
+        clipping(clipx, clipy, i);
+      } else {
+        printf("can't open (2)\n");
+        exit(1);
+      }
+    }
+  }
+}
+
---
+++ lab5.c
@@ -0,0 +1,249 @@
+#include <FPT.h>
+#include <D3d_matrix.h>
+
+double centerx, centery, centerz;
+double halfangle = 40;
+double x[100][100], y[100][100], z[100][100]; //x, y, z coordinates
+int points[100]; //number of points in whatever
+int polys[10];
+int reverse = 1;
+int shapeorder[100][100][100]; //where things connect to
+int shapes[100][100]; //the shapes[what argument][which shape]
+int WIDTH = 600; int HEIGHT = 600; int DEPTH = 600;
+
+void printarray(double *a, int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    printf("%.2lf\n", a[i]);
+  }
+  printf("\n");
+}
+
+//reads in file
+//Modified from lab2; does not read in colors
+void readobject(FILE *g, int i) {
+  int k, j;
+  fscanf(g, "%d", &points[i]);
+  // printf("there are %d points\n", points[i]);
+  for (k = 0; k < points[i]; k++) {
+    fscanf(g, "%lf  %lf  %lf", &x[i][k], &y[i][k], &z[i][k]);
+    // printf("(%lf,%lf,%lf)\n", x[i][k], y[i][k], z[i][k]);
+  }
+  fscanf(g, "%d", &polys[i]);
+  // printf("there are %d polygons in this whatever\n", polys[i]);
+  for (k = 0; k < polys[i]; k++) {
+    fscanf(g, "%d", &shapes[i][k]);
+    // printf("%d: ", shapes[i][k]);
+    for (j = 0; j < shapes[i][k]; j++) {
+      fscanf(g, "%d", &shapeorder[i][k][j]);
+      // printf("%d ", shapeorder[i][k][j]);
+    }
+    // printf("\n");
+  }
+}
+
+int check_if_behind(double *plane_x, double *plane_y, double *plane_z,
+                    int size) {
+  double p_vect[3] = {0.0};
+  double vect1[3] = {plane_x[0] - plane_x[1],
+                     plane_y[0] - plane_y[1],
+                     plane_z[0] - plane_z[1],
+                    };
+  double vect2[3] = {plane_x[0] - plane_x[2],
+                     plane_y[0] - plane_y[2],
+                     plane_z[0] - plane_z[2],
+                    };
+  double e_vect[3] = {plane_x[0] * reverse, plane_y[0] * reverse,
+                      plane_z[0] * reverse};
+  D3d_x_product(p_vect, vect1, vect2);
+  int dot_product = (p_vect[0] * e_vect[0]) + (p_vect[1] *
+                    e_vect[1]) + (p_vect[2] * e_vect[2]);
+
+  return (dot_product > 0) ? 1 : 0;
+}
+
+void drawit(int i) {
+  int k, j;
+  double tempx[points[i]], tempy[points[i]];
+  double plane_x[points[i]], plane_y[points[i]], plane_z[points[i]];
+  double mod = (HEIGHT / 2) / tan(halfangle * (M_PI / 180));
+  for (k = 0; k < polys[i]; k++) {
+    j = 0;
+
+    while (j < shapes[i][k]) {
+      // printf("%d", shapeorder[i][k][j]);
+      if (fabs(z[i][shapeorder[i][k][j]]) > 10e-7) { //Checks if z == 0
+        tempx[j] = mod * (x[i][shapeorder[i][k][j]] /
+                          z[i][shapeorder[i][k][j]]) + (WIDTH / 2);
+        tempy[j] = mod * (y[i][shapeorder[i][k][j]] /
+                          z[i][shapeorder[i][k][j]]) + (HEIGHT / 2);
+
+        plane_x[j] = x[i][shapeorder[i][k][j]];
+        plane_y[j] = y[i][shapeorder[i][k][j]];
+        plane_z[j] = z[i][shapeorder[i][k][j]];
+        j++;
+      } else {
+        j++;
+      }
+    }
+    if (check_if_behind(plane_x, plane_y, plane_z, j) == 1) {
+      G_rgb(1, 0, .3);
+      G_polygon(tempx, tempy, j);
+    }
+  }
+}
+
+//finds the largest/smallest value in array
+int findextrema(double *y, int z, int swatch) {
+  int position = 0; int i = 0;
+  double temp = 0 + (swatch * HEIGHT);
+  for (i = 0; i < z; i++) {
+    if (swatch == 0 && y[i] > temp) { //finds largest
+      position = i;
+      temp = y[i];
+    } else if (swatch == 1 && y[i] < temp) { //finds smallest
+      position = i;
+      temp = y[i];
+    }
+  }
+  return position;
+}
+
+//finds the scale factor of the object
+//also reveals the center of the object
+double scale_n_fit(int i) {
+  int k;
+  double smallx, bigx, smally, bigy, smallz, bigz;
+  bigx = x[i][findextrema(x[i], points[i], 0)];
+  smallx = x[i][findextrema(x[i], points[i], 1)];
+  bigy = y[i][findextrema(y[i], points[i], 0)];
+  smally = y[i][findextrema(y[i], points[i], 1)];
+  bigz = z[i][findextrema(z[i], points[i], 0)];
+  smallz = z[i][findextrema(z[i], points[i], 1)];
+
+  double boxheight = bigy - smally;
+  double boxwidth =  bigx - smallx;
+  double boxdepth = bigz - smallz;
+
+  centerx = (bigx + smallx) / 2;
+  centery = (bigy + smally) / 2;
+  centerz = (bigz + smallz) / 2;
+  double centers[3] = {centerx, centery, centerz};
+  return (findextrema(centers, 3, 1) == 1) ? WIDTH / boxwidth :
+         (findextrema(centers, 3, 1) == 2) ? HEIGHT / boxheight :
+         DEPTH / boxdepth;
+}
+
+//changes the message based on how many objects are inputted via command line
+void welcome(int i) {
+  printf((i == 1) ? "Press 1 to start: " :
+         (i == 2) ? "Press 1 or 2 to start: " :
+         "Press 1 through %d to start ", i);
+}
+
+int main (int argc, char **argv)
+{
+  char q, action;
+  double mat[4][4], minv[4][4], scaleFactor;
+  double radians = 3 * (M_PI / 180);
+  FILE *g;
+  int cc, sign, currentObj, k, h;
+  int increment = 15;
+  int xcounter, ycounter, zcounter = 0;
+
+  for (cc = 1; cc < argc; cc++) {
+    g = fopen(argv[cc], "r"); //opens a file; r = read only
+    if (g == NULL) { //if the file is empty, it will let me know
+      printf("can't open (1)\n");
+      exit(1);
+    } else {
+      readobject(g, cc);
+
+      D3d_make_identity(mat); D3d_make_identity(minv);
+      scaleFactor = scale_n_fit(cc);
+      D3d_translate(mat, minv, -centerx, -centery, -centerz);
+      D3d_scale(mat, minv, scaleFactor, scaleFactor, scaleFactor);
+      D3d_mat_mult_points(x[cc], y[cc], z[cc], mat,
+                          x[cc], y[cc], z[cc], points[cc]);
+      // printarray(z[cc], points[cc]);
+    }
+  }
+
+  welcome(argc - 1);
+  scanf("%c", &q);
+  currentObj = q - '0';
+  sign = 1 ;
+  action = 't' ;
+
+  if (currentObj < argc && currentObj > 0) {
+    G_init_graphics(WIDTH, HEIGHT);
+    while (1) {
+      G_rgb(0, 0, 0);
+      G_clear();
+      drawit(currentObj);
+
+      D3d_make_identity (mat) ;
+      D3d_make_identity (minv) ;
+
+      q = G_wait_key() ;
+
+      if (q == 'q') {
+        exit(0) ;
+      } else if (q == 'c') {
+        sign = -sign ;
+      } else if (q == 't') {
+        action = q ;
+      } else if (q == 's') {
+        reverse = -reverse;
+      } else if (q == 'r') {
+        action = q ;
+      } else if (('0' <= q) && (q <= '9')) {
+        k = q - '0' ;
+        if (h != currentObj) {
+          currentObj = k;
+        }
+      } else if ((q == 'x') && (action == 't')) {
+        D3d_translate (mat, minv, sign * increment, 0, 0);
+        xcounter = xcounter + (sign * increment);
+
+      } else if ((q == 'y') && (action == 't')) {
+        D3d_translate (mat, minv, 0, sign * increment, 0);
+        ycounter = ycounter + (sign * increment);
+
+      } else if ((q == 'z') && (action == 't')) {
+        D3d_translate(mat, minv, 0, 0, sign * increment);
+        zcounter = zcounter + (sign * increment);
+
+      } else if ((q == 'x') && (action == 'r')) {
+        D3d_translate(mat, minv, -xcounter, -ycounter, -zcounter);
+        D3d_rotate_x(mat, minv, sign * radians);
+        D3d_translate(mat, minv, xcounter, ycounter, zcounter);
+
+      } else if ((q == 'y') && (action == 'r')) {
+        D3d_translate(mat, minv, -xcounter, -ycounter, -zcounter);
+        D3d_rotate_y(mat, minv, sign * radians);
+        D3d_translate(mat, minv, xcounter, ycounter, zcounter);
+
+      } else if ((q == 'z') && (action == 'r')) {
+        D3d_translate(mat, minv, -xcounter, -ycounter, -zcounter);
+        D3d_rotate_z(mat, minv, sign * radians);
+        D3d_translate(mat, minv, xcounter, ycounter, zcounter);
+
+      } else {
+        printf("no action\n") ;
+      }
+
+
+      D3d_mat_mult_points (x[currentObj], y[currentObj], z[currentObj],
+                           mat,  x[currentObj], y[currentObj],
+                           z[currentObj], points[currentObj] + 1) ;
+      //the numpoints[currentObj]+1 is because we have stored
+      //the center of the object at the arrays' end
+
+      G_rgb(0, 0, 0);
+      G_clear();
+      drawit(currentObj);
+    }
+  }
+}
+
